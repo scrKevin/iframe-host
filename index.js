f6e3afb3d0b53eb867964f3d815e31a6fe213464
scrKevin <scrKevin@github.com>
2021-11-20 10:32:31 +0100

    test

---
+++ index.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
+var privateKey  = fs.readFileSync(__dirname + '/cert/privkey.pem', 'utf8');
+var certificate = fs.readFileSync(__dirname + '/cert/fullchain.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public')); 
+
+
+app.get('/twitch', function(req, res) {
+  res.render('pages/twitch/index');
+});
+
+var httpServer = http.createServer(app);
+httpServer.listen(3131);
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443);
